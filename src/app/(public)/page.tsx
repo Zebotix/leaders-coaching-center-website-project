ab65160c3e98929925dbb2377287a2fcf8603550
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2025-11-15 04:26:04 +0500

    prisma error try to resolve

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,6 @@ import WhatLeadersCoachingCenterProvides from '@/components/home/Features';
 import Hero from '@/components/home/Hero';
 import OurExpperts from '@/components/home/OurExpperts';
 import PopularCourses from '@/components/home/PopularCourses';
-import React from 'react';
 
 export default function Home() {
   return (
